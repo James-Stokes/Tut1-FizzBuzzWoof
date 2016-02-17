0991c5671ae547d7ada9adcf350a095431c2de50
James-Stokes <jstokes6699@gmail.com>
2016-02-17 22:27:26 +0200

    correct spelling of woof, change limit to 50

--- FizzBuzzWoff/hello.cpp
+++ FizzBuzzWoff/hello.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 int main() {
-	int nums[20];
-	for (int i = 0; i < 20; i++) {
+	int nums[50];
+	for (int i = 0; i < 50; i++) {
 		nums[i] = i + 1;
 	}
-	for (int i = 0; i < 20; i++) {
-		if ((nums[i] % 3) == 0) {
+	for (int i = 0; i < 50; i++) {
+		if ((nums[i] % 3) == 0 && (nums[i] % 5) == 0) {
+			cout << "Fizz Buzz" << endl;
+		}
+		else if ((nums[i] % 3) == 0) {
 			cout << "Fizz" << endl;
 		}
 		else if ((nums[i] % 5) == 0) {
 			cout << "buzz" << endl;
 		}
 		else if ((nums[i] % 7) == 0) {
-			cout << "wolf" << endl;
+			cout << "woof" << endl;
 		}
 		else {
 			cout << nums[i] << endl;
