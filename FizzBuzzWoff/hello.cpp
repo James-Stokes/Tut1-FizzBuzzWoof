a6e8a296da70837870543670988d732c390c1155
James-Stokes <jstokes6699@gmail.com>
2016-02-17 22:05:42 +0200

    add for loop to popualte array and for loop to work out if a number is fizz or buzz

---
+++ FizzBuzzWoff/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int nums[20];
+	for (int i = 0; i < 20; i++) {
+		nums[i] = i + 1;
+	}
+	for (int i = 0; i < 20; i++) {
+		if ((nums[i] % 3) == 0) {
+			cout << "Fizz" << endl;
+		}
+		else if ((nums[i] % 7) == 0) {
+			cout << "buzz" << endl;
+		}
+		else {
+			cout << nums[i] << endl;
+		}
+	}
+		return 0;
+}
