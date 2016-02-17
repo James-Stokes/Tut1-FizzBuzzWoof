f542c6e6a6305ef6e1fc84a3f6078301a133eae0
James-Stokes <jstokes6699@gmail.com>
2016-02-17 22:23:51 +0200

    add "Fizz Buzz" functionality

--- FizzBuzzWoff/hello.cpp
+++ FizzBuzzWoff/hello.cpp
@@ -8,7 +8,10 @@ int main() {
 		nums[i] = i + 1;
 	}
 	for (int i = 0; i < 20; i++) {
-		if ((nums[i] % 3) == 0) {
+		if ((nums[i] % 3) == 0 && (nums[i] % 5) == 0) {
+			cout << "Fizz Buzz" << endl;
+		}
+		else if ((nums[i] % 3) == 0) {
 			cout << "Fizz" << endl;
 		}
 		else if ((nums[i] % 5) == 0) {
