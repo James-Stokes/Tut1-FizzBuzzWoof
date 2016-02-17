1b86fb53dd280d554c8459362792f4089629b05b
James <jstokes6699@gmail.com>
2016-02-17 22:58:41 +0200

    Update hello.cpp

--- FizzBuzzWoff/hello.cpp
+++ FizzBuzzWoff/hello.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main() {
+	//array of numners to be checked, 1-20
 	int nums[20];
 	for (int i = 0; i < 20; i++) {
+		//popualtes array
 		nums[i] = i + 1;
 	}
 	for (int i = 0; i < 20; i++) {
+		//checking to see if number is divisable by 3 or 5 or both
 		if ((nums[i] % 3) == 0 && (nums[i] % 5) == 0) {
 			cout << "Fizz Buzz" << endl;
 		}
@@ -18,6 +21,7 @@ int main() {
 			cout << "buzz" << endl;
 		}
 		else {
+			//default case
 			cout << nums[i] << endl;
 		}
 	}
